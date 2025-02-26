9a049476e338a1c249c57d7ddfcb166c8ac963c7
Dennis Moldovan <dennismoldovan32@gmail.com>
2025-02-26 13:54:14 +0200

    chore - request/response adjustments

--- controllers/upload.js
+++ controllers/upload.js
@@ -15,41 +15,54 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const form_data_1 = __importDefault(require("form-data"));
 const busboy_1 = __importDefault(require("busboy"));
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
+const stream_1 = __importDefault(require("stream"));
 const upload = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //@ts-ignore
     const bb = (0, busboy_1.default)({ headers: req.headers });
-    bb.on("file", (_, file, info) => {
+    bb.on("file", (_, file, info) => __awaiter(void 0, void 0, void 0, function* () {
         const { filename, mimeType } = info;
-        const tempFilePath = path_1.default.join(__dirname, filename);
-        // Save file temporarily
-        const stream = fs_1.default.createWriteStream(tempFilePath);
-        file.pipe(stream);
-        stream.on("finish", () => __awaiter(void 0, void 0, void 0, function* () {
-            try {
-                throw new Error("plm");
-                // Prepare FormData for Discord
-                const formData = new form_data_1.default();
-                formData.append("file", fs_1.default.createReadStream(tempFilePath), { filename });
-                formData.append("payload_json", JSON.stringify({ content: "Here's an image!" }));
-                // Send to Discord
-                const discordResponse = yield axios_1.default.post(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, formData, {
-                    headers: Object.assign({ Authorization: `${process.env.BOT_TOKEN}` }, formData.getHeaders()),
-                });
-                // Cleanup temp file
-                fs_1.default.unlinkSync(tempFilePath);
-                //@ts-ignore
-                res.json({ message: "File uploaded and sent to Discord", discordResponse: discordResponse.data });
-            }
-            catch (error) {
-                console.error("Error uploading to Discord:", error);
-                //@ts-ignore
-                res.status(500).json({ error: "Failed to upload image" });
-            }
-        }));
-    });
+        const passThrough = new stream_1.default.PassThrough(); // Stream data in memory
+        // Pipe file data directly into the FormData stream
+        const formData = new form_data_1.default();
+        formData.append("file", passThrough, { filename, contentType: mimeType });
+        formData.append("payload_json", JSON.stringify({ content: "Here's an image!" }));
+        // Stream file data directly into FormData
+        file.pipe(passThrough);
+        // Send to Discord
+        try {
+            const response = yield axios_1.default
+                .post(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, formData, {
+                headers: Object.assign({ Authorization: `${process.env.BOT_TOKEN}` }, formData.getHeaders()),
+            });
+            const discordImageId = response.data.id;
+            const url = yield generateUploadLink(process.env.DISCORD_CHANNEL_ID, discordImageId);
+            //@ts-ignore
+            res.json({ message: "File uploaded and sent to Discord", discordResponse: url });
+        }
+        catch (err) {
+            console.error("Server error uploading to Discord:", err);
+            //@ts-ignore
+            res.status(500).json({ error: "Failed to upload image" });
+        }
+    }));
     //@ts-ignore
     req.pipe(bb);
 });
+function generateUploadLink(channelId, messageId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios_1.default
+                .get(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}`, {
+                headers: {
+                    Authorization: `${process.env.BOT_TOKEN}`,
+                },
+            });
+            return response.data.attachments[0].url || "";
+        }
+        catch (err) {
+            console.log("Server error: ", err);
+            return "";
+        }
+    });
+}
 module.exports = { upload };
--- controllers/upload.ts
+++ controllers/upload.ts
@@ -1,58 +1,67 @@
 import axios from "axios";
 import FormData from "form-data";
 import busboy from "busboy";
-import fs from "fs";
-import path from "path";
+import stream from "stream";
 
 
 const upload = async (req: Request, res: Response) =>{
-    //@ts-ignore
-    const bb = busboy({ headers: req.headers });
+  //@ts-ignore
+  const bb = busboy({ headers: req.headers });
 
-  bb.on("file", (_, file, info) => {
+  bb.on("file", async (_, file, info) => {
     const { filename, mimeType } = info;
-    const tempFilePath = path.join(__dirname, filename);
+    const passThrough = new stream.PassThrough(); // Stream data in memory
 
-    // Save file temporarily
-    const stream = fs.createWriteStream(tempFilePath);
-    file.pipe(stream);
+    // Pipe file data directly into the FormData stream
+    const formData = new FormData();
+    formData.append("file", passThrough, { filename, contentType: mimeType });
+    formData.append("payload_json", JSON.stringify({ content: "Here's an image!" }));
 
-    stream.on("finish", async () => {
-      try {
-    throw new Error("plm")
+    // Stream file data directly into FormData
+    file.pipe(passThrough);
 
-        // Prepare FormData for Discord
-        const formData = new FormData();
-        formData.append("file", fs.createReadStream(tempFilePath), { filename });
-        formData.append("payload_json", JSON.stringify({ content: "Here's an image!" }));
+    // Send to Discord
+    try{
+      const response = await axios
+      .post(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, formData, {
+        headers: {
+          Authorization: `${process.env.BOT_TOKEN}`,
+          ...formData.getHeaders(),
+        },
+      });
+      const discordImageId = response.data.id;
+      const url = await generateUploadLink(process.env.DISCORD_CHANNEL_ID as string,discordImageId);
 
-        // Send to Discord
-        const discordResponse = await axios.post(
-          `https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`,
-          formData,
-          {
-            headers: {
-              Authorization: `${process.env.BOT_TOKEN}`,
-              ...formData.getHeaders(),
-            },
-          }
-        );
-
-        // Cleanup temp file
-        fs.unlinkSync(tempFilePath);
-        //@ts-ignore
-        res.json({ message: "File uploaded and sent to Discord", discordResponse: discordResponse.data });
-      } catch (error) {
-        console.error("Error uploading to Discord:", error);
-        //@ts-ignore
-        res.status(500).json({ error: "Failed to upload image" });
-      }
-    });
+      //@ts-ignore
+      res.json({ message: "File uploaded and sent to Discord", discordResponse: url });
+      
+    }catch(err){
+      console.error("Server error uploading to Discord:", err);
+      //@ts-ignore
+      res.status(500).json({ error: "Failed to upload image" });
+    }
+    
   });
-
   //@ts-ignore
   req.pipe(bb);
+}
 
+
+async function generateUploadLink(channelId: string, messageId: string): Promise<string>{
+  try{
+    const response = await axios
+    .get(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}`,{
+      headers: {
+        Authorization: `${process.env.BOT_TOKEN}`,
+      },
+    });
+    return response.data.attachments[0].url || "";
+    
+  }catch(err){
+    console.log("Server error: ",err);
+    
+    return "";
+  }
 }
 
 module.exports = {upload};
